56bce7d53c6aeba34f46d79852bca246a6bf4539
shrushti2000 <shrushtipolekar@gmail.com>
2021-03-04 20:13:13 +0530

    connect server side and dialogflow

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ app.use(bodyParser.json())
 
 require('./routes/dialogFlowRoutes')(app);
 
-app.post('/api/df_text_query',(req,res)=>{
-    res.send({'do':"text query"})
-})
-app.post('/api/df_event_query',(req,res)=>{
-    res.send({'do':"event query"})
-})
+
 
 const PORT=process.env.PORT || 5000;
 app.listen(PORT);
