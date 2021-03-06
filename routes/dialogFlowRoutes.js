1af17fad3f32ea682881c65f0bf650fd6beb7f5e
shrushti2000 <shrushtipolekar@gmail.com>
2021-03-06 11:53:09 +0530

    made minor changes

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -4,7 +4,7 @@ module.exports =  app=>{
     app.post('/api/df_text_query',async (req,res)=>{
       /**let responses=await chatbot.textQuery(req.body.text,req.body.parameters) 
       res.send(responses[0].queryResult)**/
-      res. json({ message: 'Hello World'});
+      res. send({ message: 'Hello World'});
 
           });
            // Send request and log result
@@ -12,7 +12,7 @@ module.exports =  app=>{
     app.post('/api/df_event_query',async  (req,res)=>{
       //let responses=await chatbot.eventQuery(req.body.event,req.body.parameters) 
       //res.send(responses[0].queryResult)
-      res. json({ message: 'Hello World'}); 
+      res. send({ message: 'Hello World'}); 
     
     })  
 }  
