7595932e6198a9af91f32c748bfab481060478ca
shrushti2000 <shrushtipolekar@gmail.com>
2021-03-09 19:22:57 +0530

    private key

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,4 +1,6 @@
 import React,{Component} from 'react'
+import Message from './Message';
+import axios from 'axios/index';
 class Chatbot extends Component{
     constructor(props){
         super(props);
@@ -6,11 +8,53 @@ class Chatbot extends Component{
             messages:[]
         }
     }
+    async df_text_query(text){
+        let says={
+            speaks:'me',
+            msg:{
+                text:{
+                    text:text
+                }
+            }
+        }
+        this.setState({messages8:[...this.state.messages,says]})
+        const res=await axios.post('/api/df_text_query',{text})
+        for(let msg of  res.data.fullfillmentMessages){
+           let  says={
+                speaks:'bot',
+                msg:msg
+            }
+            this.setState({messages:[...this.state.messages,says]})
+        }
+    }
+    async df_event_query(event){
+        const res=await axios.post('/api/df_event_query',{event})
+        for(let msg of  res.data.fullfillmentMessages){
+           let  says={
+                speaks:'me',
+                msg:msg
+            }
+            this.setState({messages:[...this.state.messages,says]})
+        } 
+    }
+    async componentDidMount(){
+        this.df_event_query('Welcome');
+    }
+    renderMessages(stateMessages){
+        if(stateMessages){
+            return stateMessages.map((message,i)=>{
+                return <Message key={i} speaks={message.speaks} text={message.msg.text.text}/>
+            })
+        }else{
+            return null;
+        }
+    }
     render(){
         return(
             <div style={{height:400,width:400,float:'right'}}>
             <div id="chatbot" style={{height:'100%',width:'100%' ,overflow:'auto'}}>
                 <h2>Chatbot</h2>
+                {this.renderMessages(this.state.messages)}
                 <input type="text"></input>
             </div>
             </div>
